12f2780c6c6393bf50ba1709e20f5ffbc9bef64a
DESKTOP-P0CU31O\Daeyoung <daeyoung1385@gmail.com>
2025-12-04 14:41:14 +0900

    change prettier setting

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -1,3 +1,15 @@
 import vuetify from 'eslint-config-vuetify'
+import prettier from 'eslint-plugin-prettier/recommended'
 
-export default vuetify()
+export default vuetify(
+  // Vuetify config options
+  {},
+  // Additional configs
+  prettier,
+  {
+    rules: {
+      // Prettier integration - show warnings instead of errors
+      'prettier/prettier': 'warn',
+    },
+  }
+)
